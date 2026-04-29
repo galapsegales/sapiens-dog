e66d1ff064350ccea52fcc659392e84a0610c3dc
Gala P Segales <galapsegales@gmail.com>
2026-04-29 17:23:17 +0200

    Configurar GitHub Pages con dominio personalizado sapiensdog.com

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite';
 import { resolve } from 'path';
 
 export default defineConfig({
-  base: '/sapiens-dog/',
+  base: '/',
   build: {
     rollupOptions: {
       input: {
